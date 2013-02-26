d950109f14cf6185f51f3764e606f2edbd965121
Steffen Gebert <steffen.gebert@typo3.org>
2013-02-27 00:30:13 +0100

    Initial draft

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+node.set['rabbitmq']['enabled_users'] = []
+node.set['rabbitmq']['enabled_plugins'] = ["rabbitmq_management"]
---
+++ recipes/default.rb
@@ -0,0 +1,47 @@
+include_recipe "rabbitmq"
+
+node.set['rabbitmq']['erlang_cookie'] = "1q2o890ajfakjhr2kjhakjhasd"
+
+rabbitmq_plugin "rabbitmq_management" do
+  notifies :restart, "service[rabbitmq-server]"
+end
+
+vhosts = [
+  'infrastructure',
+  'infrastructure_dev'
+]
+
+vhosts.each do |vhost|
+  rabbitmq_vhost vhost
+end
+
+{
+  'bbringenberg' => 'bbringenberg',
+  'sgebert' => 'sgebert',
+  'mstucki' => 'mstucki',
+  'dev.forge.typo3.org' => 'dev.forge.typo3.org'
+}.each do |user, pass|
+
+  rabbitmq_user user do
+    password pass
+    action :add
+  end
+
+  rabbitmq_user user do
+    user_tag 'administrator'
+    action :set_user_tags
+  end
+
+  vhosts.each do |vhost_name|
+    rabbitmq_user user do
+      vhost vhost_name
+      permissions ".* .* .*"
+      action :set_permissions
+    end
+  end
+
+end
+
+rabbitmq_user 'guest' do
+  action :delete
+end
