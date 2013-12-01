7678a51d63d128e4cef8bb0fccac01c69b1f3ebb
Steffen Gebert <steffen.gebert@typo3.org>
2013-12-01 19:33:07 +0100

    Bump version to 0.0.23

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures mq.typo3.org"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.22"
+version "0.0.23"
 
 depends "rabbitmq", "~> 2.0"
 depends "nginx"
