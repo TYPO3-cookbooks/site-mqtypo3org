d4ccc78025f17030f94acd2b4be56c6333f06fdc
Steffen Gebert <steffen.gebert@typo3.org>
2013-12-15 21:58:39 +0100

    Depend on rabbitmq 2.3.3

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email "steffen.gebert@typo3.org"
 license          "Apache 2.0"
 description      "Installs/Configures mq.typo3.org"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version "0.0.27"
+version "0.0.28"
 
-depends "rabbitmq", "~> 2.0"
+depends "rabbitmq", "~> 2.3.2"
 depends "nginx"
 depends "ssl_certificates"
 depends "chef-vault"
