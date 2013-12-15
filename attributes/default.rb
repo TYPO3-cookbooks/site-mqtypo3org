55c4f9b12a6940c92f9e5a1f35284175e9c3809e
Steffen Gebert <steffen.gebert@typo3.org>
2013-12-15 20:36:59 +0100

    Set attributes with default precendence level
    I learned that this is enough in the wrapper cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,11 +1,11 @@
-override[:rabbitmq][:version] = "3.2.2"
-override[:rabbitmq][:package] = "https://www.rabbitmq.com/releases/rabbitmq-server/v#{node['rabbitmq']['version']}/rabbitmq-server_#{node['rabbitmq']['version']}-1_all.deb"
+default[:rabbitmq][:version] = "3.2.2"
+default[:rabbitmq][:package] = "https://www.rabbitmq.com/releases/rabbitmq-server/v#{node['rabbitmq']['version']}/rabbitmq-server_#{node['rabbitmq']['version']}-1_all.deb"
 
 # make sure that there's no guest account!
-override[:rabbitmq][:enabled_users] = []
-override[:rabbitmq][:disabled_users] = ['guest']
+default[:rabbitmq][:enabled_users] = []
+default[:rabbitmq][:disabled_users] = ['guest']
 
-override[:rabbitmq][:virtualhosts] = ['infrastructure', 'infrastructure_dev']
+default[:rabbitmq][:virtualhosts] = ['infrastructure', 'infrastructure_dev']
 
 default[:'site-mqtypo3org'][:hostname] = 'mq.typo3.org'
 default[:'site-mqtypo3org'][:proxy][:hostname] = 'mq.typo3.org'
