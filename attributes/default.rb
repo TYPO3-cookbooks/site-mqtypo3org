c66ce1a869ca4ab7bc75b26a6b6e04f92d50cc71
Steffen Gebert <steffen.gebert@typo3.org>
2013-12-15 20:42:30 +0100

    FC019: Access node attributes in a consistent manner

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,13 @@
 default[:rabbitmq][:version] = "3.2.2"
-default[:rabbitmq][:package] = "https://www.rabbitmq.com/releases/rabbitmq-server/v#{node['rabbitmq']['version']}/rabbitmq-server_#{node['rabbitmq']['version']}-1_all.deb"
+default[:rabbitmq][:package] = "https://www.rabbitmq.com/releases/rabbitmq-server/v#{node[:rabbitmq][:version]}/rabbitmq-server_#{node[:rabbitmq][:version]}-1_all.deb"
 
 # make sure that there's no guest account!
 default[:rabbitmq][:enabled_users] = []
-default[:rabbitmq][:disabled_users] = ['guest']
+default[:rabbitmq][:disabled_users] = ["guest"]
 
-default[:rabbitmq][:virtualhosts] = ['infrastructure', 'infrastructure_dev']
+default[:rabbitmq][:virtualhosts] = ["infrastructure", "infrastructure_dev"]
 
-default[:'site-mqtypo3org'][:hostname] = 'mq.typo3.org'
-default[:'site-mqtypo3org'][:proxy][:hostname] = 'mq.typo3.org'
-default[:'site-mqtypo3org'][:proxy][:ssl] = true
-default[:'site-mqtypo3org'][:proxy][:ssl_certificate] = 'wildcard.typo3.org'
+default[:"site-mqtypo3org"][:hostname] = "mq.typo3.org"
+default[:"site-mqtypo3org"][:proxy][:hostname] = "mq.typo3.org"
+default[:"site-mqtypo3org"][:proxy][:ssl] = true
+default[:"site-mqtypo3org"][:proxy][:ssl_certificate] = "wildcard.typo3.org"
--- recipes/proxy.rb
+++ recipes/proxy.rb
@@ -1,33 +1,33 @@
 include_recipe "nginx"
 
-if node['site-mqtypo3org']['proxy']['ssl']
+if node[:"site-mqtypo3org"][:proxy][:ssl]
 
   ssl_certfile_path = "/etc/ssl/certs/ssl-cert-snakeoil.pem"
   ssl_keyfile_path  = "/etc/ssl/certs/ssl-cert-snakeoil.key"
 
   # don't use snakeoil CA, if specified otherwise
-  if node['site-mqtypo3org']['proxy']['ssl_certificate']
-    ssl_certificate node['site-mqtypo3org']['proxy']['ssl_certificate'] do
+  if node[:"site-mqtypo3org"][:proxy][:ssl_certificate]
+    ssl_certificate node[:"site-mqtypo3org"][:proxy][:ssl_certificate] do
       ca_bundle_combined true
     end
-    ssl_certfile_path = node['ssl_certificates']['path'] + "/" + node['site-mqtypo3org']['proxy']['ssl_certificate'] + ".crt"
-    ssl_keyfile_path  = node['ssl_certificates']['path'] + "/" + node['site-mqtypo3org']['proxy']['ssl_certificate'] + ".key"
+    ssl_certfile_path = node[:ssl_certificates][:path] + "/" + node[:"site-mqtypo3org"][:proxy][:ssl_certificate] + ".crt"
+    ssl_keyfile_path  = node[:ssl_certificates][:path] + "/" + node[:"site-mqtypo3org"][:proxy][:ssl_certificate] + ".key"
   end
 end
 
 
-template "#{node[:nginx][:dir]}/sites-available/#{node['site-mqtypo3org']['proxy']['hostname']}" do
+template "#{node[:nginx][:dir]}/sites-available/#{node[:site-mqtypo3org][:proxy][:hostname]}" do
   source "nginx-site.erb"
   mode "0644"
   notifies :restart, "service[nginx]"
   variables(
-    :server_name  => node['site-mqtypo3org']['proxy']['hostname'],
+    :server_name  => node[:"site-mqtypo3org"][:proxy][:hostname],
     :ssl_certfile => ssl_certfile_path,
     :ssl_keyfile  => ssl_keyfile_path
   )
   notifies :reload, "service[nginx]"
 end
 
-nginx_site node['site-mqtypo3org']['proxy']['hostname'] do
+nginx_site node[:"site-mqtypo3org"][:proxy][:hostname] do
   enable true
 end
